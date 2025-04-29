5d96ca4b05299b8731cde7f45e1b684b7f112c1f
aaybanmonana <monanaivanvincent@gmail.com>
2025-04-29 09:32:28 +0800

    added ExtentReport

--- src/test/java/TestBase/Base.java
+++ src/test/java/TestBase/Base.java
@@ -1,10 +1,17 @@
 package TestBase;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -41,4 +48,14 @@ public class Base {
 	public void tearDown() {
 		driver.quit();
 	}
+	
+	public String getScreenshot(String methodName) throws IOException {
+		String currentTimeStamp=new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+		File srcFile =((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String targetFilepath = System.getProperty("user.dir") + "\\screenshot\\" + methodName + "_" + currentTimeStamp+".png";
+		File targetFile = new File(targetFilepath);
+		FileUtils.copyFile(srcFile, targetFile);
+		return targetFilepath;
+	}
+	
 }
